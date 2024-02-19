7ebe06f168526aa6255e625fe2db87611b199579
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-02-19 11:50:58 -0300

    update in models

--- trees/models.py
+++ trees/models.py
@@ -8,11 +8,24 @@ class Account(models.Model):
   update  = models.DateTimeField(auto_now=True)
   active  = models.BooleanField(default=True)
 
+  class Meta:
+    ordering = ('-name',)
+
+  def __str__(self):
+      return self.name
+
 
 class Profile(models.Model):
   user    = models.OneToOneField(User, on_delete=models.CASCADE)
   about   = models.TextField()
   joined  = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ('-joined',)
+
+  def __str__(self):
+    return f"{self.user.first_name}"
+
 
 
 class Tree(models.Model):
@@ -20,13 +33,27 @@ class Tree(models.Model):
   scientific_name = models.CharField(max_length=100)
 
 
+  class Meta:
+    ordering = ('-scientific_name',)
+
+  def __str__(self):
+      return self.name
+
+
 class PlantedTree(models.Model):
   user               = models.ForeignKey(User, on_delete=models.CASCADE)
   account            = models.ForeignKey(Account, on_delete=models.CASCADE,
                                          related_name='planted_trees_account')
-  tree               = models.ForeignKey(Account, on_delete=models.CASCADE,
+  tree               = models.ForeignKey(Tree, on_delete=models.CASCADE,
                                          related_name='planted_trees_tree')
   age                = models.IntegerField()
   planted_at         = models.DateTimeField(auto_now_add=True)
   location_latitude  = models.DecimalField(max_digits=9, decimal_places=6 )
   location_longitude = models.DecimalField(max_digits=9, decimal_places=6 )
+
+
+  class Meta:
+    ordering = ('-planted_at',)
+
+  def __str__(self):
+    return f"Planted Tree #{self.id}"
