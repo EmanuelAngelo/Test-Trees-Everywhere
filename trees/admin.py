5fb95ae22f57e8f2f3743e97d3d2376c4bd87b5a
Emanuel Angelo <emanuelangelo@outlook.com.br>
2024-02-19 11:50:38 -0300

    created admin.py and validacion

--- trees/admin.py
+++ trees/admin.py
@@ -1,2 +1,27 @@
 from django.contrib import admin
 from .models import Account, Profile, Tree, PlantedTree
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+  list_display = ('name','active','created')
+  list_filter  = ('active','created')
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+  list_display = ('user','joined')
+  list_filter  = ('joined',)
+
+
+@admin.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+  list_filter = ('name', 'scientific_name')
+  list_filter = ('name',)
+
+
+@admin.register(PlantedTree)
+class PlantedTreeAdmin(admin.ModelAdmin):
+  list_display = ('user','account','age','tree','location_latitude',
+                  'location_longitude',)
+  list_filter  = ('user','planted_at')
